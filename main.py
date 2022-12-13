481cf24c88384c3058166c66a2c70f6f581bb367
SocialOplesk <80935352+SocialOplesk@users.noreply.github.com>
2022-12-13 16:40:48 -0300

    feature new main

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+user_list = ["foo","bar","baz"]
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route("/users", methods=["GET"])
+def users():
+    return jsonify({
+        "payload":user_list
+    });
+    
+
+@app.route("/user/", methods=["GET"])
+def user():
+    name = request.args.get("name")
+    cb = lambda i : i == name
+    
+    try: 
+        capture = list(filter(cb,user_list))[0]
+     
+        return jsonify({
+            "payload":capture
+        })
+    except:
+        return jsonify({
+            "payload":"user not found"
+        })
+            
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
